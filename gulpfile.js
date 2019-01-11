da9d47371bd7c6355fed11fb3d57d124635ed67c
Denis Seleznev <hcodes@yandex.ru>
2019-01-11 17:03:26 +0300

    Update deps

--- gulpfile.js
+++ gulpfile.js
@@ -10,24 +10,29 @@ gulp.task('del', function() {
 	return del(browsers.map(bro => './addon_' + bro));
 });
 
-gulp.task('copy-typograf', ['del'], function() {
+gulp.task('copy-typograf', function() {
     return gulp.src([
-            'node_modules/dist/typograf.all.js'
+            'node_modules/typograf/dist/typograf.all.js'
         ])
         .pipe(gulp.dest('addon/popup/'));
 });
 
 browsers.forEach(function(browser) {
-	gulp.task(`${browser}-copy`, ['copy-typograf'], function() {
+	gulp.task(`${browser}-copy`, function() {
 		return gulp.src([`./addon/**/*`, `./${browser}/**/*`])
 			.pipe(gulp.dest(`./addon_${browser}`));
 	});
 
-	gulp.task(`${browser}-pack`, [`${browser}-copy`], function() {
+	gulp.task(`${browser}-pack`, function() {
 		return gulp.src(`./addon_${browser}/**/*`)
 			.pipe(zip(`addon_${browser}.zip`))
 			.pipe(gulp.dest('.'));
 	});
 });
 
-gulp.task('default', browsers.map(browser => browser + '-pack'));
+gulp.task('default', gulp.series(
+    'del',
+    'copy-typograf',
+    gulp.parallel(...browsers.map(browser => browser + '-copy')),
+    gulp.parallel(...browsers.map(browser => browser + '-pack'))
+));
