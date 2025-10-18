e639fec54a1d57f10997d9147192e0185bd3d4cb
Denis Seleznev <hcodes@users.noreply.github.com>
2025-10-18 16:03:17 +0300

    Fix small bugs

--- src/utils/browser.ts
+++ src/utils/browser.ts
@@ -3,7 +3,24 @@ export const isChrome = !('browser' in window);
 export function getBrowser() {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
-    const browser: typeof chrome = globalThis.browser || globalThis.chrome;
+    const browser: typeof chrome = globalThis.browser || globalThis.chrome || {
+        i18n: {
+            getUILanguage: () => 'ru',
+            getMessage: (key: string) => key,
+        },
+        storage: {
+            local: {
+                set: () => '',
+                get: (_name: string, callback?: (data: unknown) => void) => {
+                    if (callback) {
+                        callback({});
+                    }
+
+                    return Promise.resolve({});
+                },
+            },
+        },
+    };
 
     return browser;
 }
