bea3d991a58eaa5a2f77bb5c789112ee2655e348
Denis Seleznev <hcodes@users.noreply.github.com>
2025-10-18 16:00:13 +0300

    Create eslint.config.mjs

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+  {
+    ignores: [
+      '.*',
+      'addon/**',
+      'node_modules/**',
+      'dist/**',
+    ]
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts}']
+  },
+  {
+    languageOptions: { globals: {
+      ...globals.browser,
+      ...globals.node
+    }}
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
