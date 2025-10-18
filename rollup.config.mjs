232013971c1b6f4253723d7e9f0d06a2ebbcd77a
Denis Seleznev <hcodes@users.noreply.github.com>
2025-10-18 16:56:01 +0300

    Fix build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,5 +1,6 @@
 import typescript from '@rollup/plugin-typescript';
 import css from 'rollup-plugin-import-css';
+import resolve from '@rollup/plugin-node-resolve';
 
 const plugins = [typescript({ tsconfig: './tsconfig.json' })];
 
@@ -18,7 +19,12 @@ export default [
             format: 'iife',
             file: './addon/background.js'
         },
-        plugins,
+        plugins: [
+            ...plugins,
+            resolve({
+                browser: true,
+            }),
+        ]
     },
     {
         input: 'src/popup/index.ts',
